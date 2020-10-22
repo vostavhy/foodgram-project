0b2e66582af24e43791e81b3f45a3f50986c1012
Vostavhy <vostavhy@gmail.com>
2020-10-22 16:21:49 +0300

    создал все приложения, добавил первую модель

---
+++ recipes/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.db.models.fields import files
+
+User = get_user_model()
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
+    name = models.TextField()
+    description = models.TextField()
+    slug = models.SlugField(max_length=100, unique=True)
+    preparation_time = models.IntegerField()  # время приготовления в минутах
+    image = models.ImageField(upload_to='recipes_images/')  # поле для рисунка
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True, db_index=True)
+
+
