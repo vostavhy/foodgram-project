7f8485b2b06aa9e6315fb361e634b2113d47e2b5
Nick <stalcrot@yandex.ru>
2020-10-26 12:43:22 +0300

    добавил user_filters для отображения ошибок в форме регистрации

--- users/forms.py
+++ users/forms.py
@@ -10,4 +10,4 @@ class CreationForm(UserCreationForm):
         # модель уже существует, сошлемся на неё
         model = User
         # укажем, какие поля должны быть видны и в каком порядке
-        fields = ('first_name', 'last_name', 'username', 'email')
+        fields = ('first_name', 'username', 'email')
