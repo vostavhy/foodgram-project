cbd8cd2b98f913174894cbdc789a719b8b181233
Nick <stalcrot@yandex.ru>
2020-10-26 16:49:07 +0300

    добавил logout.html

--- users/urls.py
+++ users/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('signup/', views.SignUP.as_view(), name='signup'),
 
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
