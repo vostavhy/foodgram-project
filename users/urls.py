aacd04b16cee6f031fe37af87ff6a2be42ced467
Nick <stalcrot@yandex.ru>
2020-10-26 19:33:52 +0300

    добавил страницу change-form-temp

--- users/urls.py
+++ users/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 
-    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(template_name='password_change_form.html'), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
