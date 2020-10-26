26ab5153991883916e6762cde91d0e18807b1082
Nick <stalcrot@yandex.ru>
2020-10-27 01:42:20 +0300

    добавил password_reset_done

--- users/urls.py
+++ users/urls.py
@@ -22,7 +22,10 @@ urlpatterns = [
          auth_views.PasswordResetView.as_view(template_name='registration/_password_reset_form.html'),
          name='password_reset'),
 
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='registration/_password_reset_done.html'),
+         name='password_reset_done'),
+
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
