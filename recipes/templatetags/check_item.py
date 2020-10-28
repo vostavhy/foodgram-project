c5c8a549398584ce22b800f4acf33852dfd6b73d
Nick <stalcrot@yandex.ru>
2020-10-28 16:54:14 +0300

    добавил recipe_item.html

---
+++ recipes/templatetags/check_item.py
@@ -0,0 +1,21 @@
+from django import template
+from recipes.models import Follow, Order, Favorite
+register = template.Library()
+
+
+@register.filter
+def check_follow(author, user):
+    """подписан ли текущий пользователь на автора"""
+    return Follow.objects.filter(author_id=author.id, user_id=user.id).exist()
+
+
+@register.filter
+def check_order(recipe, user):
+    """присутствует ли рецепт в спике покупок пользователя"""
+    return Order.objects.filter(recipe_id=recipe.id, user_id=user.id).exist()
+
+
+@register.filter
+def check_favorite(recipe, user):
+    """присутствует ли рецепт в спике покупок пользователя"""
+    return Favorite.objects.filter(recipe_id=recipe.id, user_id=user.id).exist()
