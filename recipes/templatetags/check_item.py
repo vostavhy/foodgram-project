4a7302073443fc277e410bd95c716d54b9e29658
Nick <stalcrot@yandex.ru>
2020-10-28 18:10:09 +0300

    добавил index.html

--- recipes/templatetags/check_item.py
+++ recipes/templatetags/check_item.py
@@ -6,16 +6,16 @@ register = template.Library()
 @register.filter
 def check_follow(author, user):
     """подписан ли текущий пользователь на автора"""
-    return Follow.objects.filter(author_id=author.id, user_id=user.id).exist()
+    return Follow.objects.filter(author_id=author.id, user_id=user.id).exists()
 
 
 @register.filter
 def check_order(recipe, user):
     """присутствует ли рецепт в спике покупок пользователя"""
-    return Order.objects.filter(recipe_id=recipe.id, user_id=user.id).exist()
+    return Order.objects.filter(recipe_id=recipe.id, user_id=user.id).exists()
 
 
 @register.filter
 def check_favorite(recipe, user):
     """присутствует ли рецепт в спике покупок пользователя"""
-    return Favorite.objects.filter(recipe_id=recipe.id, user_id=user.id).exist()
+    return Favorite.objects.filter(recipe_id=recipe.id, user_id=user.id).exists()
--- recipes/views.py
+++ recipes/views.py
@@ -14,10 +14,71 @@ def index(request):
     # получаем пагинатор и номер страницы
     page, paginator = get_pagination_info(request, recipes)
 
+    title = 'Рецепты'
+    if request.user.is_authenticated:
+        title = request.user.first_name
+
     context = {
         'page': page,
         'paginator': paginator,
         'tags': tags,
+        'title': title
     }
 
     return render(request, 'index.html', context)
+
+
+def new_recipe(request):
+    return None
+
+
+def recipe_view(request):
+    return None
+
+
+def recipe_edit(request):
+    return None
+
+
+def recipe_delete(request):
+    return None
+
+
+def profile(request):
+    return None
+
+
+def profile_follow(request):
+    return None
+
+
+def profile_unfollow(request):
+    return None
+
+
+def follow_index(request):
+    return None
+
+
+def favorite_index(request):
+    return None
+
+
+def favorite_add(request):
+    return None
+
+
+def favorite_delete(request):
+    return None
+
+
+def order_index(request):
+    return None
+
+
+def order_add(request):
+    return None
+
+
+def order_delete(request):
+    return None
