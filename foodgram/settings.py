8e2ce580685eaf0284057777cf77dee409cf7cd9
Nick <stalcrot@yandex.ru>
2020-11-11 23:39:12 +0300

    отключил DEBUG

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', default=False)
+DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
 
