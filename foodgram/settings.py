c32f5620774951e5c183630e4f800d05dee2e13d
Nick <stalcrot@yandex.ru>
2020-11-12 11:11:29 +0300

    check settings

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -102,7 +102,8 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.environ.get('DB_NAME', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'NAME': os.environ.get('DB_NAME',
+                               os.path.join(BASE_DIR, 'db.sqlite3')),
         'USER': os.environ.get('DB_USER', 'user'),
         'PASSWORD': os.environ.get('DB_PASSWORD', 'password'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
